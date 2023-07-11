03041d6fc282685ca55d537b6092213d44897a39
wyf <yifu.wang@zenlayer.com>
2023-07-11 17:15:28 +0800

    feat: react-router-dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
-import React from 'react';
+import React, { Suspense } from 'react';
+import { useRoutes } from 'react-router-dom';
+import routes from './router';
 
-export default function App() {
-  return <div>App</div>;
+function App() {
+  return (
+    <div className="App">
+      <Suspense fallback="">
+        <div className="main">{useRoutes(routes)}</div>
+      </Suspense>
+    </div>
+  );
 }
+
+export default App;
---
+++ src/router/index.tsx
@@ -0,0 +1,7 @@
+import React, { lazy } from 'react';
+import { Navigate } from 'react-router-dom';
+import type { RouteObject } from 'react-router-dom';
+
+const routes: RouteObject[] = [];
+
+export default routes;
